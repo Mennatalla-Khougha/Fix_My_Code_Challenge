d3b3ac446f632578148b7dd66e940936ef49cd08
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-13 08:00:46 +0300

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""square module"""
 
 class square():
-
+    """square class"""
     width = 0
     height = 0
 
@@ -24,7 +25,6 @@ class square():
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
