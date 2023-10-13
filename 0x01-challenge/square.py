6a64b397c972b1bc98930e059c86751b6308e0c3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-13 08:05:20 +0300

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,13 +4,10 @@
 
 class Square():
     """square class"""
-    width = 0
-    height = 0
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width=0, height=0):
         """Initialize the class"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
