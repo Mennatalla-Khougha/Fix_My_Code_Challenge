f9497aa901ac6eba3ac22a0a1358b9e930b2b7bb
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-13 08:12:33 +0300

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,21 +9,22 @@ class Square():
         self.width = width
         self.height = height
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def Permiter(self):
         """perimeter of the square"""
         return 4 * self.width
 
     def __str__(self):
         """string representation"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 
 if __name__ == "__main__":
+
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.Permiter())
