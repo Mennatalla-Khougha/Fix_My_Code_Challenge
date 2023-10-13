229ff6a14ad3b3a386ebc6d1be5c417c8c946b75
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-13 08:49:23 +0300

    task 2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+User class
+"""
+
+
+class User():
+    """ User class """
+
+    def __init__(self):
+        """ Initialize the user class """
+        self.__email = None
+
+    @property
+    def email(self):
+        """ getter for email """
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        """ setter for email """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
+if __name__ == "__main__":
+
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
